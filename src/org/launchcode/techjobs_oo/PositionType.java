7b7434e26952dd15088a6d17ecd9c310d05e3888
Jessica <jrnewport07@gmail.com>
2020-01-31 10:55:06 -0600

    step 3 and fixed positionType equals and hashCode methods

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -21,21 +21,23 @@ public class PositionType {
     @Override
     public String toString(){ return value;}
 
+
+// TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
+    //  their id fields match.
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PositionType that = (PositionType) o;
-        return Objects.equals(value, that.value);
+        return id == that.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(value);
+        return Objects.hash(id);
     }
-// TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
-    //  their id fields match.
-
 
     // Getters and Setters:
 
